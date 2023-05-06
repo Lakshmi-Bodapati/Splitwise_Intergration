4ce356edc05fc5f2ad7275facaebacf2086041a8
sai kumar <bodapati.saikumar@gmail.com>
2023-05-06 16:29:44 +0100

    Variables needed for Spitwise and Gspread

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "Bucket" {
+  type = string
+  description = "Enter Name of Bucket which has gsheets JSON File"
+}
+
+variable "Bucket_key" {
+  type = string
+  description = "Enter path to the JSON file in the bucket"
+}
+
+variable "CRON" {
+    type = string
+    description = "Schedule at which sequence lambda should be running"
+}
+
+variable "split-key" {
+    type = string
+    description = "Splitwise API Key"
+}
+
+variable "Gsheet-name" {
+    type = string
+    description = "Enter Name of your google sheets"
+}
+
+variable "Place-to-insert" {
+    type = string
+    description = "Coloum to insert the value"
+}
+
+
+
+
+
+
